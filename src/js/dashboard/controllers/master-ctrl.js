7c46d41234d48b7d053b967600147b67dc749227
Craig Soich <craig.soich@ddiworld.com>
2014-12-16 16:57:42 -0500

    Added sidebar navigation functionality

--- src/js/dashboard/controllers/master-ctrl.js
+++ src/js/dashboard/controllers/master-ctrl.js
@@ -1,24 +1,63 @@
 /**
  * Master Controller
  */
+    var yearstring = "2015";
+
+var navItems = [
+        {text:"Home",path:"/",pane:"/panes/home.html"},
+        {text:yearstring+" Mainstage Season",path:"/mainstage",pane:"/panes/mainstage.html", children:[
+            {text:"play1",path:"/mainstage/play1",pane:"/panes/play1.html"},
+            {text:"play2",path:"/mainstage/play2",pane:"/panes/play2.html"},
+            {text:"play3",path:"/mainstage/play3",pane:"/panes/play3.html"},
+            {text:"play4",path:"/mainstage/play4",pane:"/panes/play4.html"},
+            {text:"play5",path:"/mainstage/play5",pane:"/panes/play5.html"},
+            {text:"play6",path:"/mainstage/play6",pane:"/panes/play6.html"},
+            {text:"play7",path:"/mainstage/play7",pane:"/panes/play7.html"}
+
+        ]},
+        {text:yearstring+" Children's Season",path:"/jact",pane:"/panes/jact.html", children:[
+            {text:"JACTplay1",path:"/jact/play1",pane:"/panes/JACTplay1.html"},
+            {text:"JACTplay2",path:"/jact/play2",pane:"/panes/JACTplay2.html"},
+            {text:"JACTplay3",path:"/jact/play3",pane:"/panes/JACTplay3.html"}
+
+        ]},
+        {text:"Ticket Information",path:"/tickets",pane:"/panes/tickets.html"}
+    ];
+var $routeProviderReference;
 var appController=angular.module('Dashboard',['ngRoute']);
+function mapRoutes (theseNavItems) {
+    var j = 0, currentRoute;
+
+    for ( ; j < theseNavItems.length; j++ ) {
+
+            currentRoute = theseNavItems[j];
+console.log(currentRoute.path,currentRoute.pane,currentRoute.children);
+            $routeProviderReference.when(currentRoute.path, {
+                    templateUrl: currentRoute.pane
+            });
+            if (currentRoute.children) {
+                mapRoutes(currentRoute.children);
+            }
+
+    }
+
+}
 appController
     .config(['$routeProvider',
     function($routeProvider) {
-      $routeProvider.
-        when('/', {
-          templateUrl: 'panes/home.html'
-        }).
-        when('/mainStage', {
-          templateUrl: 'panes/mainstage.html'
-        }).
-        otherwise({
-          redirectTo: '/'
-        });
+      $routeProviderReference = $routeProvider;
     }])
-     .controller('MasterCtrl', ['$scope',  MasterCtrl]);
+     .controller('MasterCtrl', ['$scope',  MasterCtrl])
+     .run(['$rootScope', '$route', function($rootScope, $http, $route) {
+             mapRoutes(navItems);
+		$routeProviderReference.otherwise({ redirectTo: "/" });
 
-function MasterCtrl($scope) {
+//		$route.reload();
+
+	
+}]);
+
+function MasterCtrl($scope, $http) {
     /**
      * Sidebar Toggle & Cookie Control
      *
@@ -28,13 +67,7 @@ function MasterCtrl($scope) {
 //  });
 
     var mobileView = 992;
-    var yearstring = "2015";
-    $scope.navitems=[
-        {text:"Home",path:"/",pane:"/panes/home.html"},
-        {text:yearstring+" Mainstage Season",path:"/mainstage",pane:"/panes/mainstage.html"},
-        {text:yearstring+" Children's Season",path:"/jact",pane:"/panes/jact.html"},
-        {text:"Ticket Information",path:"/tickets",pane:"/panes/tickets.html"},
-    ]
+    $scope.navItems= navItems;
     $scope.homepage=[
         {
             path:"/panes/home.html"
@@ -50,32 +83,13 @@ function MasterCtrl($scope) {
     $scope.sidenav="/panes/sidebar.html";
     $scope.getWidth = function() { return window.innerWidth; };
 
-//    $scope.$watch($scope.getWidth, function(newValue, oldValue)
-//    {
-//        if(newValue >= mobileView)
-//        {
-//            if(angular.isDefined($cookieStore.get('toggle')))
-//            {
-//                if($cookieStore.get('toggle') == false)
-//                {
-//                    $scope.toggle = false;
-//                }            
-//                else
-//                {
-//                    $scope.toggle = true;
-//                }
-//            }
-//            else 
-//            {
-//                $scope.toggle = true;
-//            }
-//        }
-//        else
-//        {
-//            $scope.toggle = false;
-//        }
+//   $scope.loadMenu = function() {
+//      $http.get('menu/all').success(function(items) {
+//         $scope.navitems = items;
+//      });
+//   };
 //
-//    });
+//   $scope.loadMenu();
 
     $scope.toggleSidebar = function() 
     {
